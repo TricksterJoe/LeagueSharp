aab0db25fa4e911eacb3cc28b3a0edbbe523ada8
HoesLeaguesharp <dimkossov.er@gmail.com>
2015-07-27 09:19:48 +0300

    Thresh

--- Slutty Thresh/Slutty Thresh/SluttyThresh.cs
+++ Slutty Thresh/Slutty Thresh/SluttyThresh.cs
@@ -15,13 +15,10 @@ using ItemData = LeagueSharp.Common.Data.ItemData;
 
 namespace Slutty_Thresh
 {
-    internal class SluttyThresh
+    internal class SluttyThresh : MenuConfig
     {
         public const string ChampName = "Thresh";
-        public const string Menuname = "Slutty Thresh";
         private static readonly Obj_AI_Hero Player = ObjectManager.Player;
-        public static Menu Config;
-        public static Orbwalking.Orbwalker Orbwalker;
         public static Spell Q, W, E, R;
         private static SpellSlot Ignite;
         private static SpellSlot FlashSlot;
@@ -44,162 +41,8 @@ namespace Slutty_Thresh
             Q.SetSkillshot(0.5f, 60f, 1900f, true, SkillshotType.SkillshotLine);
 
             FlashSlot = Player.GetSpellSlot("SummonerFlash");
-            Config = new Menu(Menuname, Menuname, true);
-            Config.AddSubMenu(new Menu("Orbwalking", "Orbwalking"));
 
-            var targetSelectorMenu = new Menu("Target Selector", "Target Selector");
-            TargetSelector.AddToMenu(targetSelectorMenu);
-            Config.AddSubMenu(targetSelectorMenu);
-
-            Orbwalker = new Orbwalking.Orbwalker(Config.SubMenu("Orbwalking"));
-
-            Config.AddSubMenu(new Menu("Drawings", "Drawings"));
-            Config.SubMenu("Drawings").AddItem(new MenuItem("Draw", "Display Drawings").SetValue(true));
-            Config.SubMenu("Drawings").AddItem(new MenuItem("qDraw", "Q Drawing").SetValue(true));
-            Config.SubMenu("Drawings").AddItem(new MenuItem("wDraw", "W Drawing").SetValue(true));
-            Config.SubMenu("Drawings").AddItem(new MenuItem("eDraw", "E Drawing").SetValue(true));
-            Config.SubMenu("Drawings").AddItem(new MenuItem("qfDraw", "Q->Flash Draw").SetValue(true));
-
-            var comboMenu = new Menu("Combo Settings (SB)", "combospells");
-            {
-                var qsettings = new Menu("Q (Death Sentence) Settings", "settings");
-                {
-                    qsettings.AddItem(new MenuItem("useQ", "Use Q (Death Sentence)").SetValue(true));
-                    qsettings.AddItem(new MenuItem("smartq", "Smart Q").SetValue(true));
-                    qsettings.AddItem(new MenuItem("useQ1", "Use Second Q").SetValue(true));
-                    qsettings.AddItem(
-                        new MenuItem("useQ2", "Use Second Q Delay (Death Leap)").SetValue(new Slider(1000, 0, 1500)));
-                    qsettings.AddItem(
-                        new MenuItem("qrange", "Q Only When Target Range >=").SetValue(new Slider(500, 0, 1040)));
-                    comboMenu.AddSubMenu(qsettings);
-
-                }
-                comboMenu.AddItem(new MenuItem("useE", "Use E (Flay)").SetValue(true));
-                comboMenu
-                    .AddItem(
-                        new MenuItem("combooptions", "E Mode").SetValue(new StringList(new[] {"Out", "In"}, 1)));
-                comboMenu.AddItem(new MenuItem("useR", "Use R (The Box)").SetValue(true));
-            }
-            Config.AddSubMenu(comboMenu);
-
-            var lantMenu = new Menu("Lantern Settings", "lantern");
-            {
-                foreach (var hero in 
-                    ObjectManager.Get<Obj_AI_Hero>()
-                        .Where(x => !x.IsEnemy)
-                        .Where(x => !x.IsMe)
-                        .Where((x => x.IsAlly)))
-                {
-                    {
-                        lantMenu.AddItem(new MenuItem("healop" + hero.ChampionName, hero.ChampionName))
-                            .SetValue(new StringList(new[] {"Lantern", "No Lantern"}));
-
-                        lantMenu.AddItem(
-                            new MenuItem("hpsettings" + hero.ChampionName, "Lantern When %HP <").SetValue(new Slider(20)));
-                    }
-
-                }
-                lantMenu.AddItem(new MenuItem("manalant", "%Mana for lantern").SetValue(new Slider(50)));
-                lantMenu.AddItem(new MenuItem("autolantern", "Auto Lantern Ally When Q hits").SetValue(false));
-            }
-
-            var laneMenu = new Menu("Lane Clear", "laneclear");
-            {
-                laneMenu.AddItem(new MenuItem("useelch", "Use E").SetValue(true));
-                // laneMenu.AddItem(new MenuItem("elchslider", "Minimum Minions For E").SetValue(new Slider(0, 1, 10)));
-            }
-            Config.AddSubMenu(laneMenu);
-
-            Config.AddSubMenu(lantMenu);
-            var flashMenu = new Menu("Flash Hook Settings", "flashf");
-            {
-                flashMenu.AddItem(new MenuItem("flashmodes", "Flash Modes")
-                    .SetValue(new StringList(new[] {"Flash->E->Q", "Flash->Q"})));
-                flashMenu.AddItem(new MenuItem("qflash", "Flash Hook").SetValue(new KeyBind('T', KeyBindType.Press)));
-            }
-
-            Config.AddSubMenu(flashMenu);
-
-            var miscMenu = new Menu("Miscellaneous (Background)", "miscsettings");
-
-            var eventMenu = new Menu("Events", "eventssettings");
-            {
-                eventMenu.AddItem(new MenuItem("useW2I", "Interrupt with W").SetValue(true));
-                eventMenu.AddItem(new MenuItem("useQW2D", "W/Q On Dashing").SetValue(true));
-            }
-
-            var itemMenu = new Menu("Item Usage", "items");
-            var shieldMenu = new Menu("Shield Usage", "shield");
-            {
-                var mountainmenu = new Menu("Face Of The Mountain", "faceof");
-                {
-                    foreach (var hero in
-                        ObjectManager.Get<Obj_AI_Hero>()
-                            .Where(x => x.IsAlly
-                                        && !x.IsMe))
-                    {
-                        {
-                            mountainmenu.AddItem(new MenuItem("faceop" + hero.ChampionName, hero.ChampionName))
-                                .SetValue(new StringList(new[] {"Use", "No Use"}));
-
-                            mountainmenu.AddItem(
-                                new MenuItem("facehp" + hero.ChampionName, "Use When %HP <").SetValue(new Slider(20)));
-                        }
-                    }
-                }
-
-                var locketmenu = new Menu("Locket Of Solari", "locksol");
-                {
-                    foreach (var hero in
-                        ObjectManager.Get<Obj_AI_Hero>()
-                            .Where(x => x.IsAlly
-                                        && !x.IsMe))
-                    {
-                        {
-                            locketmenu.AddItem(new MenuItem("locketop" + hero.ChampionName, hero.ChampionName))
-                                .SetValue(new StringList(new[] {"Use", "No Use"}));
-
-                            locketmenu.AddItem(
-                                new MenuItem("lockethp" + hero.ChampionName, "Use When %HP <").SetValue(new Slider(20)));
-                        }
-                    }
-
-                }
-                shieldMenu.AddSubMenu(locketmenu);
-                shieldMenu.AddSubMenu(mountainmenu);
-            }
-
-            var healMenu = new Menu("Healing Items", "heals");
-            {
-                var mikaelss = new Menu("Mikael's Crucibile", "mikaels");
-                mikaelss.AddItem(new MenuItem("charm", "Charm").SetValue(true));
-                mikaelss.AddItem(new MenuItem("snare", "Snare").SetValue(true));
-                mikaelss.AddItem(new MenuItem("taunt", "Taunt").SetValue(true));
-                mikaelss.AddItem(new MenuItem("suppression", "Suppression").SetValue(true));
-                mikaelss.AddItem(new MenuItem("stun", "Stun").SetValue(true));
-                //  mikaelss.AddItem(new MenuItem("mikaelshp", "Use On %HP", true).SetValue(new Slider(20, 40)));
-                var allies = new Menu("Ally Config", "AllysConfig");
-                foreach (var hero in
-                    ObjectManager.Get<Obj_AI_Hero>()
-                        .Where(x => x.IsAlly
-                                    && !x.IsMe))
-                {
-                    {
-                        allies.AddItem(new MenuItem("healmikaels" + hero.ChampionName, hero.ChampionName))
-                            .SetValue(new StringList(new[] {"Use Mikaels", "Don't Use Mikaels"}));
-                    }
-                }
-                mikaelss.AddSubMenu(allies);
-                healMenu.AddSubMenu(mikaelss);
-            }
-            itemMenu.AddSubMenu(shieldMenu);
-            itemMenu.AddSubMenu(healMenu);
-            Config.AddSubMenu(itemMenu);
-
-            miscMenu.AddSubMenu(eventMenu);
-            Config.AddSubMenu(miscMenu);
-
-            Config.AddToMainMenu();
+            CreateMenuu();
 
             Drawing.OnDraw += Drawing_OnDraw;
             Game.OnUpdate += Game_OnUpdate;
@@ -346,9 +189,8 @@ namespace Slutty_Thresh
                     if (hero.HealthPercent <= Config.Item("hpsettings" + hero.ChampionName).GetValue<Slider>().Value)
                     {
                         if (hero.Distance(Player) <= W.Range
-                            && !hero.IsRecalling())
+                            && target.IsValidTarget(1400))
                         {
-                            if (target.IsValidTarget(Q.Range))
                                 W.Cast(hero.Position);
                         }
                     }
