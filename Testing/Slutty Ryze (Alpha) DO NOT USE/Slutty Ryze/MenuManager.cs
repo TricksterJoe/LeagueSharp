312676c19c466690c107039a9a934ee2a82128e7
TheReal0x0539 <TheOnly0x0539@gmail.com>
2015-07-31 04:49:23 -0700

    Removed Beta combo
    Cuz crash

--- Testing/Slutty Ryze (Alpha) DO NOT USE/Slutty Ryze/MenuManager.cs
+++ Testing/Slutty Ryze (Alpha) DO NOT USE/Slutty Ryze/MenuManager.cs
@@ -78,7 +78,7 @@ namespace Slutty_ryze
                 combo1Menu
                     .AddItem(
                         new MenuItem("combooptions", "Combo Mode").SetValue(
-                            new StringList(new[] {"Stable", "Beta Combo"})));
+                            new StringList(new[] {"Improved Combo System"})));
                 combo1Menu.AddItem(new MenuItem("useQ", "Use Q (Over Load)").SetValue(true));
                 combo1Menu.AddItem(new MenuItem("useW", "Use W (Rune Prison)").SetValue(true));
                 combo1Menu.AddItem(new MenuItem("useE", "Use E (Spell Flux)").SetValue(true));
