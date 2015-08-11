03b052d7338f747962bf824041dc42b000dbd95b
TheReal0x0539 <TheOnly0x0539@gmail.com>
2015-08-11 03:42:46 -0700

    TEst 1

--- Testing/Slutty Ryze (Alpha) DO NOT USE/Slutty Ryze/FastDraw.cs
+++ Testing/Slutty Ryze (Alpha) DO NOT USE/Slutty Ryze/FastDraw.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using LeagueSharp;
 using SharpDX;
 
@@ -12,7 +10,7 @@ namespace Slutty_ryze
     {
         static FastDraw()
         {
-            Quality = 1.5f;
+            Quality = .1f;
         }
 
         public static float Quality { get; set; }
