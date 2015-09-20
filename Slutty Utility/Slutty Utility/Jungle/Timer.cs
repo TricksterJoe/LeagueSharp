b51b54cefa95b6722ea653522849dc49bf1dc9d4
KallenStadtfeldGeass <KallenStadtfeldGeass@gmail.com>
2015-09-20 00:35:43 -0700

    Jungle Work
    Work in process.

--- Slutty Utility/Slutty Utility/Jungle/Timer.cs
+++ Slutty Utility/Slutty Utility/Jungle/Timer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,8 +10,30 @@ using LeagueSharp.Common;
 
 namespace Slutty_Utility.Jungle
 {
-    internal class Timer
+    internal class Timer : Helper
     {
+        protected float _jungleTick = 0f;
 
+        private void OnCreate(GameObject sender, EventArgs args)
+        {
+            if (!sender.IsValid) return;
+            if(sender.Type != GameObjectType.obj_AI_Minion)return;
+            if (sender.Team != GameObjectTeam.Neutral) return;
+            foreach (var camp in JungleMonsters.JungleCamps)
+            {
+                var mob = JungleMonsters.JungleCamps.FirstOrDefault(m => m.Key.ToLower().Contains(sender.Name.ToLower()));
+                if(mob.Key == null)continue;
+
+            }
+        }
+        private void Game_OnUpdate()
+        {
+            if (_jungleTick > TickCount) return;
+            _jungleTick = TickCount + 1000;
+            if(JungleMonsters.JungleCamps == null) JungleMonsters.LoadCamps();
+
+
+
+        }
     }
 }
