e9bf4acdb2fdb41884389df8fa81cb098f57c81c
HoesLeaguesharp <dimkossov.er@gmail.com>
2015-09-23 08:07:07 +0300

    OnUpdate

--- Slutty Utility/Slutty Utility/Tracker/TrackerSpell.cs
+++ Slutty Utility/Slutty Utility/Tracker/TrackerSpell.cs
@@ -30,46 +30,46 @@ namespace Slutty_Utility.Tracker
 
         private static void OnDraw(EventArgs args)
         {
-                foreach (var hero in HeroManager.AllHeroes.Where(x => x.Distance(Player) < 3000))
+            foreach (var hero in HeroManager.AllHeroes.Where(x => x.Distance(Player) < 3000))
+            {
+                for (var i = 0; i <= _spellslot.Count(); i++)
                 {
-                    for (var i = 0; i <= _spellslot.Count(); i++)
+                    X = (int) hero.HPBarPosition.X + (i*30) + 35;
+
+                    Y = (int) hero.HPBarPosition.Y - 10;
+                    Drawing.DrawText(X - 1, Y - 13, Color.AliceBlue,
+                        _spellslot[i].ToString());
+                    foreach (var spell in _spellslot)
                     {
-                        X = (int) hero.HPBarPosition.X + (i*30) + 35;
+                        var expires = (hero.Spellbook.GetSpell(_spellslot[i]).CooldownExpires);
+                        var CD =
+                            (int)
+                                (expires -
+                                 (Game.Time - 1));
 
-                        Y = (int) hero.HPBarPosition.Y - 10;
-                        Drawing.DrawText(X - 1, Y - 13, Color.AliceBlue,
-                            _spellslot[i].ToString());
-                        foreach (var spell in _spellslot)
+                        var expiress = expires - (Game.Time + 0.3);
+                        var CDPercent = expiress*100/CD;
+                        Game.PrintChat(CDPercent.ToString());
+                        if (CD > 0)
                         {
-                            var expires = (hero.Spellbook.GetSpell(_spellslot[i]).CooldownExpires);
-                            var CD =
-                                (int)
-                                    (expires -
-                                     (Game.Time - 1));
-
-                            var expiress = expires - (Game.Time + 0.3);
-                            var CDPercent = expiress*100/CD;
-                            Game.PrintChat(CDPercent.ToString());
-                            if (CD > 0)
-                            {
-                                Drawing.DrawText(X, Y,
-                                    CDPercent >= 80
-                                        ? Color.Blue
-                                        : CDPercent >= 50 && CDPercent < 80
-                                            ? Color.BlueViolet
-                                            : CDPercent < 50 && CDPercent >= 30
-                                                ? Color.LightSeaGreen
-                                                : Color.LawnGreen, CD.ToString());
-                            }
-                            else
-                            {
-                                Drawing.DrawText(X, Y, Color.Black, "0", 100);
-                            }
+                            Drawing.DrawText(X, Y,
+                                CDPercent >= 80
+                                    ? Color.Blue
+                                    : CDPercent >= 50 && CDPercent < 80
+                                        ? Color.BlueViolet
+                                        : CDPercent < 50 && CDPercent >= 30
+                                            ? Color.LightSeaGreen
+                                            : Color.LawnGreen, CD.ToString());
+                        }
+                        else
+                        {
+                            Drawing.DrawText(X, Y, Color.Black, "0", 100);
                         }
                     }
                 }
-                }
-
+            }
         }
+
     }
+}
 
