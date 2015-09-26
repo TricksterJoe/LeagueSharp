5f064378b7be18b8c0bd4aecc9c8b6bc2162dbc1
HoesLeaguesharp <dimkossov.er@gmail.com>
2015-09-26 06:15:55 +0300

    Revert "Critical Changes"
    This reverts commit 3f499e31187a5cb1c18a9b5cc1fd09e87313c368.

--- Slutty Veigar/Slutty Veigar/MenuConfig.cs
+++ Slutty Veigar/Slutty Veigar/MenuConfig.cs
@@ -97,10 +97,8 @@ namespace Slutty_Veigar
             }
             Config.AddSubMenu(drawingmenu);
 
-            Config.AddItem(new MenuItem("autoq", "Auto Q"))
-                .SetValue(new StringList(new[] {"2 Minions", "1 Minion", "Dont Auto Q"}));
 
-
+            
         }
     }
 }
