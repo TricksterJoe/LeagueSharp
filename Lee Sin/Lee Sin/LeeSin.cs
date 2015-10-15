75e91d449ea6a5605b055f81ffc6e7f56c11ec87
HoesLeaguesharp <dimkossov.er@gmail.com>
2015-10-16 00:17:16 +0300

    q prediction

--- Lee Sin/Lee Sin/LeeSin.cs
+++ Lee Sin/Lee Sin/LeeSin.cs
@@ -95,7 +95,7 @@ namespace Lee_Sin
             W = new Spell(SpellSlot.W, 700);
             E = new Spell(SpellSlot.E, 350);
             R = new Spell(SpellSlot.R, 375);
-            Q.SetSkillshot(0f, 30f, 2000f, true, SkillshotType.SkillshotLine);
+            Q.SetSkillshot(0.3f, 30f, 2000f, true, SkillshotType.SkillshotLine);
 
             foreach (var spell in Player.Spellbook.Spells)
             {
