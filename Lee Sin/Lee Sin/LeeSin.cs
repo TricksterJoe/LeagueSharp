0c45f4f74826252e24ef969e6af24c240b2b1748
HoesLeaguesharp <dimkossov.er@gmail.com>
2015-12-04 01:59:27 +0200

    Fix attempt v2.

--- Lee Sin/Lee Sin/LeeSin.cs
+++ Lee Sin/Lee Sin/LeeSin.cs
@@ -1376,14 +1376,7 @@ namespace Lee_Sin
             var slot = Items.GetWardSlot();
 
             #endregion
-            var poss = Insec(target, 320, true);
-            if (Steps == steps.WardJump && slot != null && W.IsReady() && R.IsReady())
-            {
-                if (target.Distance(Player) < 600)
-                {
-                    WardJump(poss.To3D(), false, false);
-                }
-            }
+
 
             if (Player.Distance(target) > 500)
             {
@@ -1398,33 +1391,41 @@ namespace Lee_Sin
                 Q.Cast(qpred.CastPosition);
             }
 
-            foreach (
-                var min in
-                    MinionManager.GetMinions(Player.Position, Q.Range, MinionTypes.All, MinionTeam.Enemy)
-                        .Where(
-                            x =>
-                                x.Health > GetQDamage(x) + 5 && (x.Distance(target) < 400 || x.Distance(poss) < 400) &&
-                                !x.IsDead
-                                && Q.GetPrediction(x).CollisionObjects.Count == 0)) 
-            {
-                Render.Circle.DrawCircle(min.Position, 80, Color.Yellow, 5, true);
-                if (col.Count > 0)
-                {
-                    if (Q1())
-                    {
-                        Q.Cast(min);
-                    }
+            //foreach (
+            //    var min in
+            //        MinionManager.GetMinions(Player.Position, Q.Range, MinionTypes.All, MinionTeam.Enemy)
+            //            .Where(
+            //                x =>
+            //                    x.Health > GetQDamage(x) + 5 && (x.Distance(target) < 400 || x.Distance(poss) < 400) &&
+            //                    !x.IsDead
+            //                    && Q.GetPrediction(x).CollisionObjects.Count == 0)) 
+            //{
+            //    Render.Circle.DrawCircle(min.Position, 80, Color.Yellow, 5, true);
+            //    if (col.Count > 0)
+            //    {
+            //        if (Q1() && Q.IsReady())
+            //        {
+            //            Q.Cast(min);
+            //        }
 
-                    if (Q2() && min.HasBuff("blindmonkqtwo"))
-                    {
-                        Q.Cast();
-                    }
+            //        if (Q2() && min.HasBuff("blindmonkqtwo"))
+            //        {
+            //            Q.Cast();
+            //        }
+            //    }
+            //}
+
+            var poss = Insec(target, 320, true);
+
+            if (Steps == steps.WardJump && slot != null && W.IsReady() && R.IsReady())
+            {
+                if (target.Distance(Player) < 600)
+                {
+                    WardJump(poss.To3D(), false, false);
                 }
             }
 
-
-
-                var wardtotargetpos = Player.ServerPosition.Extend(target.ServerPosition, Player.Distance(target) - 180);
+            var wardtotargetpos = Player.ServerPosition.Extend(target.ServerPosition, Player.Distance(target) - 180);
             var wardFlashBool = GetBool("expwardflash", typeof (bool));
 
 
@@ -1467,7 +1468,7 @@ namespace Lee_Sin
 
            if (R.IsReady())
             {
-                if (slot != null && (W.IsReady()  || Environment.TickCount - lastprocessw < 2000 )&& slot.IsValidSlot())
+                if (slot != null && (W.IsReady()  || Environment.TickCount - lastprocessw < 2000 )  )
                 {
                     if (GetBool("prioflash", typeof(bool)) && Player.GetSpellSlot("summonerflash").IsReady())
                     {
@@ -1477,7 +1478,6 @@ namespace Lee_Sin
                     else
                     {
                         Steps = steps.WardJump;
-                      //  Game.PrintChat("It's Ward time");
                         lastwardjump = Environment.TickCount;
                     }
                 }
