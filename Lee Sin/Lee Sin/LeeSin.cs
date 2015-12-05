3c3259f4ea1e069830b6c186f0828715cbe20c90
HoesLeaguesharp <dimkossov.er@gmail.com>
2015-12-05 16:05:21 +0200

    testing phase

--- Lee Sin/Lee Sin/LeeSin.cs
+++ Lee Sin/Lee Sin/LeeSin.cs
@@ -1339,15 +1339,14 @@ namespace Lee_Sin
             var wardtotargetpos = Player.ServerPosition.Extend(target.ServerPosition, Player.Distance(target) - 250);
             var wardFlashBool = GetBool("expwardflash", typeof (bool));
 
-            if (slot == null || !HasFlash() || !W.IsReady() ||
+            if (slot == null || !W.IsReady() ||
                 !(Player.ServerPosition.Distance(target.ServerPosition) > 350) || !(target.Distance(Player) < 1000) ||
-                !R.IsReady() || !wardFlashBool || !canwardflash) return;
+                 !wardFlashBool || !canwardflash) return;
 
             //if ((Environment.TickCount - _lastqcasted > 1000) || (col.Count > 0 && !Q2() && Environment.TickCount - _lastqcasted > 1000 && !Q.IsReady()) || (Environment.TickCount - _lastflashward < 1500))
             //{
             if (Environment.TickCount - _wardjumpedto > 1000 && R.IsReady() &&
-                ((Player.Position.Distance(target.Position) > 600) || (minions.Distance(Player) > 600 &&
-                 !minions.HasBuff("blindmonkqtwo"))))
+                ((Player.Position.Distance(target.Position) > 600) || (minions.Distance(Player) > 600 && minions.HasBuff("blindmonkqtwo"))))
             {
                 WardJump(wardtotargetpos, false, false);
 
