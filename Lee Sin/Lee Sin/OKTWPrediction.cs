159e9c0a1e3902e96718c5ffd5df92434cb7fb61
HoesLeaguesharp <dimkossov.er@gmail.com>
2016-01-15 12:20:17 +0200

    OKTW UPDATED BOIS

--- Lee Sin/Lee Sin/OKTWPrediction.cs
+++ Lee Sin/Lee Sin/OKTWPrediction.cs
@@ -4,7 +4,7 @@
  Copyright 2014 - 2014 LeagueSharp
  Prediction.cs is part of LeagueSharp.Common.
  
- LeagueSharp.Common is free software: you can redistribute it and/or modify
+ LeagueSharp.Common is free softwasre: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
