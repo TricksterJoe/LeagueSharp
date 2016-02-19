7e92f15aca1430918b56fb4cd25611b6686ee63f
HoesLeaguesharp <dimkossov.er@gmail.com>
2016-02-19 02:42:02 +0200

    ward jump range adjustment

--- Slutty Katarina/Slutty Katarina/Katarina.cs
+++ Slutty Katarina/Slutty Katarina/Katarina.cs
@@ -307,7 +307,7 @@ namespace Slutty_Katarina
             if (GetBool("wardjump", typeof (KeyBind)))
             {
                 Player.IssueOrder(GameObjectOrder.MoveTo, Game.CursorPos);
-                WardJump.WardJumped(Player.Position.Extend(Game.CursorPos, 590));
+                WardJump.WardJumped(Player.Position.Extend(Game.CursorPos, 600));
             }
 
           //  Game.PrintChat((Environment.TickCount - lastnoenemies).ToString());
