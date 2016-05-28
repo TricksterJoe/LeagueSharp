2a915432f60400fac56067543bd75df7ab9684aa
HoesLeaguesharp <dimkossov.er@gmail.com>
2016-05-28 18:14:19 +0300

    Chat Blocker - complete credit to Spark!
    Hawk just requested me to upload it.

---
+++ ChatBlocker/ChatBlocker/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using LeagueSharp;
+using LeagueSharp.Common;
+
+namespace ChatBlocker
+{
+
+    internal class Program
+    {
+        private static void Main(string[] array)
+        {
+            CustomEvents.Game.OnGameLoad += delegate
+            {
+                Game.OnChat += args => args.Process = false;
+                Game.OnInput += args => args.Process = false;
+            };
+        }
+    }
+}
