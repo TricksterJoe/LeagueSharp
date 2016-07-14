c89dfd26ef0e991b9a89bfbffae5d9ea5492423f
HoesLeaguesharp <dimkossov.er@gmail.com>
2016-07-14 18:20:31 +0300

    ops 2.0

--- Slutty Ryze/Slutty Ryze/LaneOptions.cs
+++ Slutty Ryze/Slutty Ryze/LaneOptions.cs
@@ -319,20 +319,21 @@ namespace Slutty_ryze
             {
                 Champion.Q.Cast(target);
             }
-                
-            if (Champion.W.IsReady() && target.IsValidTarget(Champion.W.Range) &&
-                (!Champion.Q.IsReady() || Champion.Q.GetPrediction(target).CollisionObjects.Count != 0)) 
-            {
-                if ((Champion.E.IsReady() && Champion.W.IsReady() && !Champion.Q.IsReady()))
-                    return;
-                Champion.W.Cast(target);
-            }
 
             if (Champion.E.IsReady() && target.IsValidTarget(Champion.E.Range) &&
                 (!Champion.Q.IsReady() || Champion.Q.GetPrediction(target).CollisionObjects.Count != 0))
             {
                 Champion.E.Cast(target);
             }
+
+            if (Champion.W.IsReady() && target.IsValidTarget(Champion.W.Range) &&
+                (!Champion.Q.IsReady() || Champion.Q.GetPrediction(target).CollisionObjects.Count != 0)) 
+            {
+                if (Champion.E.IsReady() && Champion.W.IsReady() && !Champion.Q.IsReady())
+                    return;
+                Champion.W.Cast(target);
+            }
+
 
             //if ((!target.HasBuff("RyzeE") && Champion.E.IsReady()) ||
             //    (Champion.E.IsReady() && target.IsValidTarget(Champion.E.Range)) ||
